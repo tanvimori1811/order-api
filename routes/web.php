9c0c4996595ca3c1c2b9a379168c16c8006ce73d
tanvimori1811 <tanvimori70454091@gmail.com>
2026-06-29 15:11:06 +0530

    Update README and final cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\OrderController;
 
-Route::put('/orders/{id}/status', [OrderController::class, 'updateStatus']);
-Route::get('/orders/{id}/history', [OrderController::class, 'history']);
+Route::get('/', function () {
+    return view('welcome');
+});
